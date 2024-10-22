8639a32b71182c26e7eee743c993c253aa8443f6
Zeenat_zeni <abdul_rehman2405@gmail.com>
2024-10-22 13:13:29 +0500

    start-react

---
+++ wodwes/src/App.js
@@ -0,0 +1,13 @@
+import  './index.css';
+function App() {
+  return (
+
+    <>
+    <h1 className="bg-green-500  text-3xl font-bold underline ">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App;
