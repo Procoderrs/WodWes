e8c13b2788e6f6b9b98023a8bd7f5cf0d121b5d0
Zeenat_zeni <abdul_rehman2405@gmail.com>
2024-10-23 15:39:27 +0500

    changes

--- wodwes/src/App.js
+++ wodwes/src/App.js
@@ -1,11 +1,15 @@
 import  './index.css';
+import Navbar from "./Nav/Navbar";
 function App() {
   return (
 
     <>
-    <h1 className="bg-green-500  text-3xl font-bold underline ">
-      Hello world!
-    </h1>
+ 
+  
+
+  <Navbar/>
+
+
     </>
   )
 }
---
+++ wodwes/src/Nav/Navbar.js
@@ -0,0 +1,46 @@
+ 
+ import {Link} from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';  // Corrected import
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';  // Corrected import
+import MegaMenu from "./MegaMenu";
+ import  './index.css';
+
+const Navbar=()=>{
+  
+  return(
+    
+    <>
+    
+
+<div className="flex justify-center items-center h-full w-full mx-auto  bg-black my-0 p-4">
+  <ul className="flex justify-between items-center gap-8 text-center text-white px-1.5 w-max text-xl">
+    <li>
+      <span><a href="#" className="underline">  Get a Quote</a> </span>
+      <span>today to claim $200 free credit.</span>
+    </li>
+    <li>
+<span className='mr-2'> <FontAwesomeIcon icon={faPhone} /> {/* FontAwesome icon */}
+</span>
+    +923061070678
+    </li>
+    <li> <span className='mr-2'>
+    <FontAwesomeIcon icon={faEnvelope} />
+    </span>
+    sales@wodwes.com</li>
+  </ul>
+</div>
+
+<header className="nav__header">
+<a href="#">
+        <img src='/wodweslogo.png' alt="" className=" w-44" />
+      </a> 
+<div className="hidden md:block">
+  <MegaMenu/>
+</div>
+</header>
+
+</>
+  )
+};
+export default Navbar;
